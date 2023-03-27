e0043a639691a0413a921ac36cde2c1b215eb348
Sabi <43244088+Sleron@users.noreply.github.com>
2023-03-27 17:33:43 +1300

    improving trig function classes
    adding a parent by the name TPMS and let Gyroid and others enjoy that

--- minisurf/trig.py
+++ minisurf/trig.py
@@ -1,13 +1,10 @@
 from numpy import sin, cos, pi
 
-
-class Gyroid:
+class TPMS:
     def __init__(self):
         pass
-    def __call__(self, x, y, z, ax=1, by=1, cz=1):
-        return ax*sin(2*pi*x)*cos(2*pi*y) + by*sin(2*pi*y)*cos(2*pi*z) + cz*sin(2*pi*z)*cos(2*pi*x)
-    
     def get_bunch(self, values):
+ 
         x = values[:,0]
         y = values[:,1]
         z = values[:,2]
@@ -16,37 +13,30 @@ class Gyroid:
         cz = values[:,4]
         by = values[:,5]
         return self.__call__(x,y,z,ax,by,cz)
+        
     
-class Primitive:
+class Gyroid(TPMS):
     def __init__(self):
-        pass
+        super(Gyroid, self).__init__()
+    def __call__(self, x, y, z, ax=1, by=1, cz=1):
+        return ax*sin(2*pi*x)*cos(2*pi*y) + by*sin(2*pi*y)*cos(2*pi*z) + cz*sin(2*pi*z)*cos(2*pi*x)
+    
+ 
+    
+class Primitive(TPMS):
+    def __init__(self):
+        super(Primitive, self).__init__()
+
     def __call__(self, x, y, z, ax=1, by=1, cz=1):
         return ax*cos(2*pi*x) + by*cos(2*pi*y) + cz*cos(2*pi*z)
         
     
-    def get_bunch(self, values):
-        x = values[:,0]
-        y = values[:,1]
-        z = values[:,2]
-        
-        ax = values[:,3]
-        cz = values[:,4]
-        by = values[:,5]
-        return self.__call__(x,y,z,ax,by,cz)
+
     
-class FisherS:
+class FisherS(TPMS):
     def __init__(self):
-        pass
+        super(FisherS, self).__init__()
+
     def __call__(self, x, y, z, ax=1, by=1, cz=1):
         return ax*cos(2*2*pi*x)*sin(2*pi*y)*cos(2*pi*z) + by*cos(2*pi*x)*cos(2*2*pi*y)*sin(2*pi*z) +cz*sin(2*pi*x)*cos(2*pi*y)*cos(2*2*pi*z)
         
-    
-    def get_bunch(self, values):
-        x = values[:,0]
-        y = values[:,1]
-        z = values[:,2]
-        
-        ax = values[:,3]
-        cz = values[:,4]
-        by = values[:,5]
-        return self.__call__(x,y,z,ax,by,cz)    
