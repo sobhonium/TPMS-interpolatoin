06fd71bac44684d8f6e3dc89dedcfb61ba6c21f1
Sabi <43244088+Sleron@users.noreply.github.com>
2023-04-06 15:25:04 +1200

    change name

--- minisurf/trig.py
+++ minisurf/trig.py
@@ -1,27 +1,27 @@
 from numpy import sin, cos, pi
 
+
 class TPMS:
     def __init__(self):
         pass
-    def get_bunch(self, values):
- 
-        x = values[:,0]
-        y = values[:,1]
-        z = values[:,2]
         
-        ax = values[:,3]
-        cz = values[:,4]
-        by = values[:,5]
-        return self.__call__(x,y,z,ax,by,cz)
+    def get_bunch(self, values):
+        x = values[:, 0]
+        y = values[:, 1]
+        z = values[:, 2]
+        ax = values[:, 3]
+        cz = values[:, 4]
+        by = values[:, 5]
+        
+        return self.__call__(x, y, z, ax, by, cz)
         
     
 class Gyroid(TPMS):
     def __init__(self):
         super(Gyroid, self).__init__()
+        
     def __call__(self, x, y, z, ax=1, by=1, cz=1):
         return ax*sin(2*pi*x)*cos(2*pi*y) + by*sin(2*pi*y)*cos(2*pi*z) + cz*sin(2*pi*z)*cos(2*pi*x)
-    
- 
     
 class Primitive(TPMS):
     def __init__(self):
@@ -30,13 +30,11 @@ class Primitive(TPMS):
     def __call__(self, x, y, z, ax=1, by=1, cz=1):
         return ax*cos(2*pi*x) + by*cos(2*pi*y) + cz*cos(2*pi*z)
         
-    
-
-    
 class FisherS(TPMS):
     def __init__(self):
         super(FisherS, self).__init__()
 
     def __call__(self, x, y, z, ax=1, by=1, cz=1):
-        return ax*cos(2*2*pi*x)*sin(2*pi*y)*cos(2*pi*z) + by*cos(2*pi*x)*cos(2*2*pi*y)*sin(2*pi*z) +cz*sin(2*pi*x)*cos(2*pi*y)*cos(2*2*pi*z)
-        
+        return (ax*cos(2*2*pi*x)*sin(2*pi*y)*cos(2*pi*z) +
+                by*cos(2*pi*x)*cos(2*2*pi*y)*sin(2*pi*z) +
+                cz*sin(2*pi*x)*cos(2*pi*y)*cos(2*2*pi*z))
